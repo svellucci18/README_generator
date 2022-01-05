cfe62dc31cd4b6c4983c4ab315347a297bf56914
svell <svellucci18@gmail.com>
2022-01-04 16:20:54 -0800

    Created list of questions and included necessary packages

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -10,4 +12,75 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
+
+
+// Snag the prompt from inquirer docs and add in questions
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'languages',
+      },
+      {
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'project',
+      },
+      {
+        type: 'input',
+        message: "Please write a short description of your project.",
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: "What kind of license should your project have?",
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: "What command should be run to install dependencies?",
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: "What command should be run to run tests?",
+        name: 'tests',
+      },
+      {
+        type: 'input',
+        message: "What does the user need to know about using the repo?",
+        name: 'repoUseGuide',
+      },
+      {
+        type: 'input',
+        message: "What does the user need to know about contributing to the repo?",
+        name: 'contributionGuide',
+      },
+  ])
+  .then((response) => 
+    // Use user feedback for... whatever!!
+    fs.appendFile(
+        "READMEtest.md",
+        `#${response.project}
+        ##${response.description}
+        ## Table of Contents
+        \n` , (err) =>
+        err ? console.error(err) : console.log('Generating README')
+    ))
+
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
